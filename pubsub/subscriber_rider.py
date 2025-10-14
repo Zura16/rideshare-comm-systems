95b04c7c12abd8974a0b77bdc63a6ee0797e4a8a
medinaalexis <Alexis.Medina01@student.csulb.edu>
2025-10-13 23:47:03 -0700

    pub sub

--- pubsub/subscriber_rider.py
+++ pubsub/subscriber_rider.py
@@ -1,1 +1,10 @@
-# Subscriber Placeholder
+import socket
+
+HOST, PORT = "127.0.0.1", 5001
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((HOST, PORT))
+print(f"listening on udp://{HOST}:{PORT}")
+#listen forever
+while True:
+    data, addr = sock.recvfrom(65535)
+    print("recv from", addr, data.decode("utf-8", errors="replace"))
