108997fa83fab1e62f9676ce6be29a9f624c5230
AalindCS <kaleaalind7@gmail.com>
2025-10-13 19:54:05 -0700

    demo_client

--- rest/demo_client.py
+++ rest/demo_client.py
@@ -1,1 +1,5 @@
 # Demo Client Placeholder
+import requests
+print("Requesting a ride...")
+res= requests.post("http://127.0.0.1:5001/request_ride", json={"lat":33.00, "lon":-118.10})
+print(res.json())
